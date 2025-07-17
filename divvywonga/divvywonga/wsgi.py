ea3134284c3b7e526e2a1f617acea9be0c680c17
Eryk Malczyk <eryk.malczyk@neontri.com>
2025-07-17 16:13:30 +0200

    Add django project setup

---
+++ divvywonga/divvywonga/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for divvywonga project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'divvywonga.settings')
+
+application = get_wsgi_application()
